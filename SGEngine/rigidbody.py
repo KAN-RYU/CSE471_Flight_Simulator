fix: infinite rotation

SGEngine/rigidbody.py
@@ -66,10 +66,10 @@ class RigidBody(Component):
         self.angular_velocity += np.cross(point - self.position, impulse) / self.inertia
 
     def applyRelativeTorque(self, torque):
-        self.torque += torque
+        self.torque += self.object.getLocalVec(torque)
     
     def applyRelativeTorqueStep(self, torque):
-        delta_angular_velocity = self.inverse_inertia @ torque
+        delta_angular_velocity = self.inverse_inertia @ self.object.getLocalVec(torque)
         self.angular_velocity += delta_angular_velocity
 
         quaternion_rate = Quaternion(0, *self.angular_velocity) * self.rotation

main.py
@@ -11,27 +11,29 @@ import numpy as np
 if __name__ == "__main__":
     mainScene = Scene(1280, 720)
     
-    # cube = Cube(position=np.array([0, 0.5, 0]), rotation=np.array([0, 0, 0]))
-    # rb = RigidBody(inertia=np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
+    cube = Cube()
+    rb = RigidBody(position=np.array([0, 100, 0], dtype=np.float32), inertia=np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]))
     # rb.hasGravity = False
-    # rb.mass = 1000
-    # cube.addComponent(rb)
-    # cube.addComponent(AirplaneController())
+    rb.mass = 100
+    cube.addComponent(rb)
+    cube.addComponent(AirplaneController())
     
     # cube.addComponent(FPSController())
     # plane = Plane(scale=np.array([10000, 1, 10000]))
-    camera_obj = GameObject(position=np.array([-128, 64, -128]), rotation=np.array([np.pi/8, np.pi/4, 0]))
+    # camera_obj = GameObject(position=np.array([-128, 64, -128]), rotation=np.array([np.pi/8, np.pi/4, 0]))
+    camera_obj = GameObject(position=np.array([0, 2, -6]), rotation=np.array([np.pi/8, 0, 0]))
     camera_obj.addComponent(Camera(far = 256))
-    camera_obj.addComponent(FPSController())
+    # camera_obj.addComponent(FPSController())
     # cube.addChild(camera_obj)
     mainScene.setCamera(camera_obj)
-    mainScene.addObject(camera_obj)
+    cube.addChild(camera_obj)
+    # mainScene.addObject(camera_obj)
     
     terrain = Terrain(position=np.array([0.0, 0.0, 0.0]), rotation=np.array([0.0, 0.0, 0.0]))
     
     mainScene.addObject(terrain)
     
-    # mainScene.addObject(cube)
+    mainScene.addObject(cube)
     # mainScene.addObject(plane)
     
     mainScene.run()

scripts/airplane.py
@@ -13,7 +13,7 @@ def calculateLiftCoef(x):
     return -(1/216000) * x * (x - 90) * (x + 90)
 
 def calculateSteeringCoef(x):
-    return 1 / (1 + np.exp(-1 * x))
+    return 0.1 / (1 + np.exp(-1 * x))
 
 def getScale6(value: np.ndarray(3), posX, negX, posY, negY, posZ, negZ) -> np.ndarray(3):
     result = value
@@ -43,8 +43,9 @@ class AirplaneController(Script):
         self.hInput = 0
         self.vInput = 0
         self.zInput = 0
+        self.rollInput = 0
         self.liftPower = 50
-        self.rudderPower = 20
+        self.rudderPower = 10
         self.inducedDrag = 1
         self.airBrakeDrag = 10
         self.flapsDrag = 10
@@ -52,7 +53,7 @@ class AirplaneController(Script):
         self.flapsAoaBias = 5
         self.airBrakeEnabled = False
         self.flapsEnabled = False
-        self.turnSpeed = np.array([30, 15, 270])
+        self.turnSpeed = np.array([60, 30, 270])
         self.turnAcceleration = np.array([60, 30, 540])
 
         self.velocity = np.zeros(3)
@@ -72,7 +73,7 @@ class AirplaneController(Script):
             self.rb = rb
 
     def Update(self, gameObject: GameObject, dt: float):
-        print(gameObject.position)
+        print(self.object.position)
         self.CalculateState(dt)
         self.CalculateAoA()
         self.calculateGForce(dt)
@@ -93,6 +94,10 @@ class AirplaneController(Script):
             self.hInput = 1
         if key == b'e':
             self.zInput = 1 - self.zInput
+        if key == b';':
+            self.rollInput = -1
+        if key == b'\'':
+            self.rollInput = 1
 
         if key == b' ':
             self.airBrakeEnabled = True
@@ -105,6 +110,8 @@ class AirplaneController(Script):
             self.vInput = 0
         if key == b'a' or key == b'd':
             self.hInput = 0
+        if key == b';' or key == b'\'':
+            self.rollInput = 0
 
         if key == b' ':
             self.airBrakeEnabled = False
@@ -202,11 +209,12 @@ class AirplaneController(Script):
         speed = np.max([0, self.localVelocity[2]])
         steeringPower = calculateSteeringCoef(speed)
 
-        targetAV = np.array([self.vInput, 0, -self.hInput] * self.turnSpeed * steeringPower)
+        targetAV = np.array([self.vInput, self.hInput, self.rollInput] * self.turnSpeed * steeringPower)
         av = np.rad2deg(self.localAngularVelocity)
 
         correction = np.array([self.calculateSteering(dt, av[0], targetAV[0], self.turnAcceleration[0] * steeringPower),
                                  self.calculateSteering(dt, av[1], targetAV[1], self.turnAcceleration[1] * steeringPower),
                                  self.calculateSteering(dt, av[2], targetAV[2], self.turnAcceleration[2] * steeringPower)])
         
-        self.rb.applyRelativeTorque(np.deg2rad(correction))
+        # print(correction)
+        self.rb.applyRelativeTorqueStep(np.deg2rad(correction))