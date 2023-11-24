c24f69279409f723d3b97fbc19262c836f5e7f69
chan-hwi <gazebo5@naver.com>
2023-11-25 01:33:24 +0900

    feat: camera controller

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 from SGEngine.scene import Scene
 from SGEngine.gameobject import *
 from SGEngine.camera import Camera
-from SGEngine.rigidbody import RigidBody
 from SGEngine.quaternion import *
-from SGEngine.skybox import Skybox
-from scripts.fps import FPSController
-from scripts.airplane import AirplaneController
 from scripts.terrain import Terrain
 from scripts.camera import CameraController
 from SGEngine.airplane import Airplane
 import numpy as np
-import pywavefront
 
 if __name__ == "__main__":
     mainScene = Scene(1280, 720)
@@ -18,14 +13,15 @@ if __name__ == "__main__":
 
     camera_obj = GameObject(position=np.array([0, 2, 6]), rotation=np.array([-np.pi/8, 0, 0]))
     camera_obj.addComponent(Camera(far = 4096))
+    camera_obj.addComponent(CameraController(airplane))
 
     mainScene.setCamera(camera_obj)
-    airplane.addChild(camera_obj)
 
     terrain = Terrain(position=np.array([0.0, 0.0, 0.0]), rotation=np.array([0.0, 0.0, 0.0]))
     
     mainScene.addObject(terrain)
     mainScene.addObject(airplane)
+    mainScene.addObject(camera_obj)
     
     mainScene.run()
 
--- scripts/camera.py
+++ scripts/camera.py
@@ -9,8 +9,11 @@ class CameraController(Script):
         self.target = target
         self.alpha = 0.5
         self.rotalpha = 0.1
-        self.rotoffset = np.array([np.pi / 8, 0, 0])
-        self.offset = np.array([0, 2, -6])
+        self.rotoffset = np.array([-np.pi / 8, 0, 0])
+        self.offset = np.array([0, 2, 6])
 
     def Update(self, obj: GameObject, dt: float):
-        pass
+        obj.setPosition(self.target.position + self.target.getLocalVec(self.offset))
+        rot = self.target.rotation * Quaternion.from_euler(*self.rotoffset)
+        rot.normalize()
+        obj.setRotation(rot)
